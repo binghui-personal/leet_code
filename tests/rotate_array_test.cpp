103bfd00a21c85a02b68bf370647228149c18929
李炳辉 <kartlbh@aliyun.com>
2026-03-10 18:20:45 +0800

    test: 增加更多的测试（空数组，k为0，负数，超出数组大小等）

--- tests/rotate_array_test.cpp
+++ tests/rotate_array_test.cpp
@@ -20,6 +20,14 @@ TEST(RotateArrayTest, BruteForce2) {
     EXPECT_EQ(nums, expected);
 }
 
+TEST(RotateArrayTest, BruteForce3) {
+    Solution s;
+    std::vector<int> nums = {-1,-100,3,99};
+    s.rotateBruteForce(nums, -3);
+    std::vector<int> expected = {99,-1,-100,3};
+    EXPECT_EQ(nums, expected);
+}
+
 TEST(RotateArrayTest, TripleReverse1) {
     Solution s;
     std::vector<int> nums = {1,2,3,4,5,6,7};
@@ -33,6 +41,62 @@ TEST(RotateArrayTest, TripleReverse2) {
     std::vector<int> nums = {-1,-100,3,99};
     s.rotateTripleReverse(nums, 2);
     std::vector<int> expected = {3,99,-1,-100};
+    EXPECT_EQ(nums, expected);
+}
+
+TEST(RotateArrayTest, TripleReverse3) {
+    Solution s;
+    std::vector<int> nums = {-1,-100,3,99};
+    s.rotateTripleReverse(nums, 4);
+    std::vector<int> expected = {-1,-100,3,99};
+    EXPECT_EQ(nums, expected);
+}
+
+TEST(RotateArrayTest, TripleReverse4) {
+    Solution s;
+    std::vector<int> nums = {};
+    s.rotateTripleReverse(nums, 4);
+    std::vector<int> expected = {};
+    EXPECT_EQ(nums, expected);
+}
+
+TEST(RotateArrayTest, TripleReverse5) {
+    Solution s;
+    std::vector<int> nums = {};
+    s.rotateTripleReverse(nums, 0);
+    std::vector<int> expected = {};
+    EXPECT_EQ(nums, expected);
+}
+
+TEST(RotateArrayTest, TripleReverse6) {
+    Solution s;
+    std::vector<int> nums = {};
+    s.rotateTripleReverse(nums, -4);
+    std::vector<int> expected = {};
+    EXPECT_EQ(nums, expected);
+}
+
+TEST(RotateArrayTest, TripleReverse7) {
+    Solution s;
+    std::vector<int> nums = {-1,-100,3,99};
+    s.rotateTripleReverse(nums, -1);
+    std::vector<int> expected = {-100, 3, 99, -1};
+    EXPECT_EQ(nums, expected);
+}
+
+TEST(RotateArrayTest, TripleReverse8) {
+    Solution s;
+    std::vector<int> nums = {-1,-100,3,99};
+    s.rotateTripleReverse(nums, 4);
+    std::vector<int> expected = {-1,-100,3,99};
+    EXPECT_EQ(nums, expected);
+}
+
+TEST(RotateArrayTest, TripleReverse9) {
+    Solution s;
+    std::vector<int> nums = {-1,-100,3,99};
+    s.rotateTripleReverse(nums, 5);
+    std::vector<int> expected = {99,-1,-100,3};
     EXPECT_EQ(nums, expected);
 }
 
